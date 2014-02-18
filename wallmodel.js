d2de02ad6383e79fde19a4a670a881f838c94235
PH <perhenrik.kvalnes@gmail.com>
2014-02-18 16:13:44 +0100

    started on wall model and added image

---
+++ wallmodel.js
@@ -0,0 +1,12 @@
+/*****************************
+ * WALL MODEL
+ * 
+ * @author Per-Henrik Kvalnes
+ *
+ ******************************/
+function createWall()
+{
+	obj = new Object();
+
+	return obj;
+}
