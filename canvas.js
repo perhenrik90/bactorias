d3db105546a52d914d7a220651e745a49618e06e
Per-Henrik <perhenrik.kvalnes@gmail.com>
2014-02-19 14:34:07 +0100

    added offset for the view

--- canvas.js
+++ canvas.js
@@ -5,11 +5,18 @@
  ********************************/
 updateRate = 40;
 blockWidth = 32;
+blockHeight = 32;
 
 function gameCanvas( canvas )
 {
-	// get context 
+	// get context
 	ctx = canvas.getContext("2d");
+
+	// init view variables
+	viewposx = 100;
+	viewposy = 0;
+
+	// create models
 	player = createPlayerModel();
 	player.setX(250);
 	player.setY(10);
@@ -21,29 +28,35 @@ function gameCanvas( canvas )
 	{
 		ctx.clearRect( 0, 0, canvas.width, canvas.height);
 
-		x = player.xpos;
-		y = player.ypos;
+		x = player.xpos - viewposx;
+		y = player.ypos - viewposy;
 		i = player.imgid;
 
+		//
+		// DRAW PLAYER
+		//
 		img = new Image();
 		img.src = "img/bakterie.png";
 	
 		ctx.drawImage(img, x, y);
  
-		/** draw world **/
+		// 
+		// DRAW WORLD
+		//
 		imgWall = new Image();
 		imgWall.src = "img/wall.png";
 
+
+		/** go though the wall list **/
 		for(i = 0; i < world.length; i ++)
 		{
 			wall 	= world[i];
-			x1	= wall.xpos*blockWidth;
-			y1	= wall.ypos*blockWidth;
+			x1	= (wall.xpos*blockWidth)-viewposx;
+			y1	= (wall.ypos*blockWidth)-viewposy;
 		
-			x2	= x1 + blockWidth;
-			y2	= y1 + blockWidth;
+			x2	= blockWidth;
+			y2	= blockHeight;
 
-			console.log("X1: "+ x1 + " X2: "+x2);
 			ctx.drawImage(imgWall, x1, y1, x2, y2);
 		}
 	}
@@ -65,8 +78,28 @@ function gameCanvas( canvas )
 	//
 	// CONTROLLER CODE
 	//
+
+	/** push the player upwards when canvas clicked **/
 	canvas.onclick = function(e)
 	{
 		player.pushForce();
 	}
+
+	//
+	// SET FUNCTIONS
+	//
+
+	/** set the view position **/
+	function setViewX(x)
+	{
+		viewposx = x;
+		this.onDraw();
+	}
+	function setViewY(y)
+	{
+		viewposx = y;
+		this.onDraw();
+
+	}	
+			
 }
